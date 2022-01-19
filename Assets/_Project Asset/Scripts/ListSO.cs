2775773424680928d8d0887ec3afcb25fcdff577
BahaaSrour <58504971+BahaaSrour@users.noreply.github.com>
2022-01-19 02:48:31 +0200

    Save And Load Data

--- Assets/_Project Asset/Scripts/ListSO.cs
+++ Assets/_Project Asset/Scripts/ListSO.cs
@@ -1,8 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-public class ListSO<T> : ScriptableObject where T : class
+public class ListSO<T> : ScriptableObject where T : NameClass
 {
     public List<T> itemsList;
     public int Count { get => itemsList.Count; }
+
+    public T GetItemByName(string name)
+    {
+        for (int i = 0; i < itemsList.Count; i++)
+        {
+            if (itemsList[i].name == name) return itemsList[i];
+        }
+        return null;
+    }
 }
--- Assets/_Project Asset/Scripts/Shape.cs
+++ Assets/_Project Asset/Scripts/Shape.cs
@@ -1,14 +1,18 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class Shape : MonoBehaviour
 {
     static Shape instance;
+    static string JsonFilePath;
+
     public ShapeColor shapeColor;
     public ShapeMesh shapeMesh;
-
+    public ShapesList shapesList;
+    public ColorList colorList;
     public static Shape Instance
     {
         get { return instance; }
@@ -18,17 +22,54 @@ public class Shape : MonoBehaviour
     {
         if (instance == null)
             instance = this;
+        JsonFilePath = Application.dataPath + "Settings.Json";
+        Load();
     }
 
+    public void Save()
+    {
+        if (File.Exists(JsonFilePath))
+            File.Delete(JsonFilePath);
+        File.WriteAllText(JsonFilePath, JsonUtility.ToJson(new ShapeData { ColorName = shapeColor.name , MeshName = shapeMesh.name }));
+    }
 
+    public void Load()
+    {
+        if (File.Exists(JsonFilePath))
+        {
+            var tmp= JsonUtility.FromJson<ShapeData>(File.ReadAllText(JsonFilePath));
+            this.shapeColor = colorList.GetItemByName( tmp.ColorName);
+            this.shapeMesh = shapesList.GetItemByName( tmp.MeshName);
+            UpdateShapeProperties();
+        }
+    }
     public void UpdateShapeProperties()
     {
         GetComponent<MeshFilter>().mesh = shapeMesh.shapeMesh;
         GetComponent<Renderer>().material.color = shapeColor.shapeColor;
+    }
+    private void OnDestroy()
+    {
+        Save();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.S))
+            Save();
+        if (Input.GetKey(KeyCode.L))
+            Load();
     }
 }
 
 public class NameClass
 {
     public string name;
+}
+
+[System.Serializable]
+public struct ShapeData
+{
+    public string MeshName;
+    public string ColorName;
 }
